f21289799a48134cc7908f1820a01123e8a64474
agdavid <antonio.g.david@gmail.com>
2019-11-10 12:11:38 -0500

    add mapbox request

--- weather-app/index.js
+++ weather-app/index.js
@@ -2,10 +2,12 @@ const request = require('request');
 const chalk = require('chalk');
 const env = require('dotenv').config({ path: '../.env' })
 
-const url = `https://api.darksky.net/forecast/${process.env.DARKSKY_SECRET_KEY}/37.8267,-122.4233?`;
+
+const mapboxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAPBOX_ACCESS_TOKEN}&limit=1`
+const darkSkyUrl = `https://api.darksky.net/forecast/${process.env.DARKSKY_SECRET_KEY}/37.8267,-122.4233?`;
 
 request({
-    url,
+    url: darkSkyUrl,
     json: true
 }, (error, response) => {
     const { currently, daily } = response.body;
@@ -13,4 +15,17 @@ request({
 
     console.log(chalk.green(`${daily.data[0].summary}`));
     console.log(chalk.green(`It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of ${precipType || 'precipitation'}. `));
+});
+
+request({
+    url: mapboxUrl,
+    json: true
+}, (error, response) => {
+    const { features } = response.body;
+    const lat = features[0].center[1];
+    const lng = features[0].center[0];
+
+    console.log(features[0].text);
+    console.log(`Lat: ${lat}`);
+    console.log(`Lng: ${lng}`);
 });
