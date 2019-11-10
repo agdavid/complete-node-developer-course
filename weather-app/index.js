d7d47e8dbc5979d9763fe5538b7864181b956ef5
agdavid <antonio.g.david@gmail.com>
2019-11-10 11:36:01 -0500

    parse json with request pkg

--- weather-app/index.js
+++ weather-app/index.js
@@ -4,8 +4,9 @@ const env = require('dotenv').config({ path: '../.env' })
 const url = `https://api.darksky.net/forecast/${process.env.DARKSKY_SECRET_KEY}/37.8267,-122.4233`;
 
 request({
-    url
+    url,
+    json: true
 }, (error, response) => {
-    const data = JSON.parse(response.body);
+    const data = response.body;
     console.log(data.currently);
 });
