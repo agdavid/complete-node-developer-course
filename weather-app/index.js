3b5f99da057d2dc46e3946ed83c7886a86c60bdd
agdavid <antonio.g.david@gmail.com>
2019-11-10 11:53:11 -0500

    add darksky api options

--- weather-app/index.js
+++ weather-app/index.js
@@ -1,13 +1,16 @@
 const request = require('request');
+const chalk = require('chalk');
 const env = require('dotenv').config({ path: '../.env' })
 
-const url = `https://api.darksky.net/forecast/${process.env.DARKSKY_SECRET_KEY}/37.8267,-122.4233`;
+const url = `https://api.darksky.net/forecast/${process.env.DARKSKY_SECRET_KEY}/37.8267,-122.4233?`;
 
 request({
     url,
     json: true
 }, (error, response) => {
-    const data = response.body;
-    const { temperature, precipProbability, precipType } = data.currently;
-    console.log(`It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of ${precipType || 'precipitation'}. `)
+    const { currently, daily } = response.body;
+    const { temperature, precipProbability, precipType } = currently;
+
+    console.log(chalk.green(`${daily.data[0].summary}`));
+    console.log(chalk.green(`It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of ${precipType || 'precipitation'}. `));
 });
