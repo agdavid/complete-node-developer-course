69b71b46adfcdebcc32100be9af1407ee645614c
agdavid <antonio.g.david@gmail.com>
2019-11-10 16:16:58 -0500

    es6 objects playground

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,34 @@
+// const name = 'Andrew';
+
+// const age = 27;
+
+// const user = {
+//     name,
+//     age,
+//     location: 'Philadelphia'
+// };
+
+// console.log(user);
+
+const product = {
+    label: 'Red notebook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined
+};
+
+// const { label, price: productPrice, stock, salePrice, rating, fake = 5 } = product;
+
+// console.log(label);
+// console.log(rating);
+// console.log(productPrice);
+// console.log(fake);
+
+const transaction = (type, { label, stock }) => {
+    // const { label } = myProduct;
+    console.log(type);
+    console.log(label);
+    console.log(stock);
+}
+
+transaction('order', product);
