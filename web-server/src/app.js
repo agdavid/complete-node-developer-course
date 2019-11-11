e21b68025dd9561854b2f85eaf7e4b2d1d0b69b4
agdavid <antonio.g.david@gmail.com>
2019-11-10 22:00:38 -0500

    custome hbs views directory

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,10 +2,17 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.set('view engine', 'hbs'); // set templating engine for /views
-app.use(express.static(publicDirectoryPath)); // serve static files
+// define pathes for express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+// setup handlebars
+app.set('view engine', 'hbs'); // set templating engine
+app.set('views', viewsPath); // point to custom views directory instead of /views
+
+// setup static directory to serve
+app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
     res.render('index', {
@@ -18,14 +25,14 @@ app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About',
         name: 'Thomas'
-    })
-})
+    });
+});
 
 app.get('/help', (req, res) => {
     res.render('help', {
         message: "We\'ll help you right away!"
-    })
-})
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
