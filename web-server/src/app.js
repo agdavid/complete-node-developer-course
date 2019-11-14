25fd1acb903669de9e4972af100611229cb41b62
agdavid <antonio.g.david@gmail.com>
2019-11-13 21:36:45 -0500

    pass query string param to endpoint

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -40,9 +40,17 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
+
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        });
+    }
+
     res.send({
         forecast: 'Mostly sunny',
-        location: 'Atlanta'
+        location: 'Atlanta',
+        address: req.query.address
     });
 });
 
