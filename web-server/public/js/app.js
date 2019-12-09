cdedb857fccb41af34268ed3403b5a508a971844
agdavid <antonio.g.david@gmail.com>
2019-12-08 22:20:14 -0500

    render error/success of fetch to index page

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,10 +2,14 @@ console.log('Client side JS file loaded');
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-one');
+const messageTwo = document.querySelector('#message-two');
 
 weatherForm.addEventListener('submit', e => {
   e.preventDefault();
   const location = search.value;
+  messageOne.textContent = 'Fetching weather...';
+  messageTwo.textContent = '';
   fetchWeather(location);
 });
 
@@ -13,9 +17,10 @@ const fetchWeather = location => {
   fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
     response.json().then(data => {
       if (data.error) {
-        console.log(data.error);
+        messageOne.textContent = data.error;
       } else {
-        console.log(data);
+        messageOne.textContent = data.location;
+        messageTwo.textContent = `${data.forecast.summary} The temperature is ${data.forecast.temperature} degrees and a ${data.forecast.precipProbability}% chance of precipitation.`;
       }
     });
   });
