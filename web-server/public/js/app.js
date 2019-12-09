274588f16f4679e04e59b16a258e44442647f442
agdavid <antonio.g.david@gmail.com>
2019-12-08 21:41:23 -0500

    wire up weather fetch form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,11 +1,22 @@
-console.log("Client side JS file loaded");
+console.log('Client side JS file loaded');
 
-fetch("http://localhost:3000/weather?address=!").then(response => {
-  response.json().then(data => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data);
-    }
-  });
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const location = search.value;
+  fetchWeather(location);
 });
+
+const fetchWeather = location => {
+  fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data);
+      }
+    });
+  });
+};
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,51 +1,51 @@
-const path = require("path");
-const express = require("express");
-const hbs = require("hbs");
-const env = require("dotenv").config({ path: "../.env" });
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const env = require('dotenv').config({ path: '../.env' });
 
 const app = express();
 
-const { geocode, forecast } = require("./utils.js");
+const { geocode, forecast } = require('./utils.js');
 
 // define pathes for express config
-const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname, "../templates/partials");
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // setup handlebars
-app.set("view engine", "hbs"); // set templating engine
-app.set("views", viewsPath); // point to custom views directory instead of /views
+app.set('view engine', 'hbs'); // set templating engine
+app.set('views', viewsPath); // point to custom views directory instead of /views
 hbs.registerPartials(partialsPath);
 
 // setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-app.get("", (req, res) => {
-  res.render("index", {
-    title: "Weather App",
-    name: "Thomas"
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Thomas'
   });
 });
 
-app.get("/about", (req, res) => {
-  res.render("about", {
-    title: "About",
-    name: "Thomas"
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About',
+    name: 'Thomas'
   });
 });
 
-app.get("/help", (req, res) => {
-  res.render("help", {
-    title: "Help",
-    name: "Thomas",
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    name: 'Thomas',
     message: "We'll help you right away!"
   });
 });
 
-app.get("/weather", (req, res) => {
+app.get('/weather', (req, res) => {
   if (!req.query.address) {
     return res.send({
-      error: "You must provide an address"
+      error: 'You must provide an address'
     });
   }
 
@@ -75,23 +75,23 @@ app.get("/weather", (req, res) => {
 });
 
 // add wildcard for sub-route of /help
-app.get("/help/*", (req, res) => {
-  res.render("404", {
-    title: "404",
-    name: "Thomas",
-    message: "Help article not found"
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Thomas',
+    message: 'Help article not found'
   });
 });
 
 // add wildcard route as catch-all for 404
-app.get("*", (req, res) => {
-  res.render("404", {
-    title: "404",
-    name: "Thomas",
-    message: "Page not found"
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Thomas',
+    message: 'Page not found'
   });
 });
 
 app.listen(3000, () => {
-  console.log("Server is up on Port 3000");
+  console.log('Server is up on Port 3000');
 });
