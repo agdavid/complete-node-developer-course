63b16f14176173462ca6242c3ca5f7ceb0ab1069
agdavid <antonio.g.david@gmail.com>
2019-12-17 17:59:39 -0500

    add git to notes-app

--- notes-app/index.js
+++ notes-app/index.js
@@ -12,62 +12,58 @@ const notes = require('./notes.js');
 */
 
 yargs
-    .usage('Usage: node $0 <command> [options]')
-    .command(
-        {
-            command: 'add',
-            describe: 'Add a note',
-            builder: {
-                title: {
-                    describe: 'Note title',
-                    demandOption: true,
-                    type: 'string'
-                },
-                body: {
-                    describe: 'Note body',
-                    demandOption: true,
-                    type: 'string'
-                }
-            },
-            handler(argv) {
-                notes.addNote(argv.title, argv.body);
-            }
-        })
-    .command(
-        {
-            command: 'remove',
-            describe: 'Remove a note',
-            builder: {
-                title: {
-                    describe: 'Note title',
-                    demandOption: true,
-                    type: 'string'
-                }
-            },
-            handler(argv) {
-                notes.removeNote(argv.title);
-            }
-        })
-    .command(
-        {
-            command: 'read',
-            describe: 'Read a note',
-            builder: {
-                title: {
-                    describe: 'Note title',
-                    demandOption: true,
-                    type: 'string'
-                }
-            },
-            handler(argv) {
-                notes.readNote(argv.title);
-            }
-        })
-    .command(
-        {
-            command: 'list',
-            describe: 'List all notes',
-            handler() {
-                notes.listNotes();
-            }
-        }).argv;
+  .usage('Usage: node $0 <command> [options]')
+  .command({
+    command: 'add',
+    describe: 'Add a note',
+    builder: {
+      title: {
+        describe: 'Note title',
+        demandOption: true,
+        type: 'string'
+      },
+      body: {
+        describe: 'Note body',
+        demandOption: true,
+        type: 'string'
+      }
+    },
+    handler(argv) {
+      notes.addNote(argv.title, argv.body);
+    }
+  })
+  .command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+      title: {
+        describe: 'Note title',
+        demandOption: true,
+        type: 'string'
+      }
+    },
+    handler(argv) {
+      notes.removeNote(argv.title);
+    }
+  })
+  .command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+      title: {
+        describe: 'Note title',
+        demandOption: true,
+        type: 'string'
+      }
+    },
+    handler(argv) {
+      notes.readNote(argv.title);
+    }
+  })
+  .command({
+    command: 'list',
+    describe: 'List all notes',
+    handler() {
+      notes.listNotes();
+    }
+  }).argv;
